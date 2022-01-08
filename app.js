0ab6b83d42a38ba6a8eaa9560a2ee86da801a221
plotinusspascual <plotinusspascual@gmail.com>
2022-01-07 18:25:24 -0800

    removed unecessary code that was added initially on accident

--- app.js
+++ app.js
@@ -1,136 +1,49 @@
-$(document).ready(function () {
+$('.saveBtn').on("click",function(){
+  console.log(this);
+  //save user input to corresponding container
+  var userText = $(this).siblings(".userInput").val();
+  var userTime = $(this).parent().attr("id");
 
-    // Static
-
-    // The first basic method of Moment.js is the moment() method. Use this to get today's info!
-    console.log(moment());
-
-    console.log(moment().format());
-
-    // Using moment format, there are different ways that we can display todays date!
-    $("#date").text(moment());
-    $("#date-formatted").text(moment().format());
-    $("#date-my").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
-
-    $("#day-week").text(moment().format('do'));
-    $("#day-month").text(moment().format('Do'));
-    $("#day-year").text(moment().format('DDDo'))
-
-    // Current Time
-    // Here we want to use setInterval to constantly update the time
-    let updateTime = function () {
-        let currentTime = moment().format('h:mm:ss')
-        $("#time").text(currentTime)
-    }
-
-    // Here we will get the number of hours in the year
-    let updateHour = function () {
-        let date = moment().dayOfYear()
-        let yearHours = date * 24
-        let todayHours = moment().hour();
-        let sumHours = yearHours + todayHours
-        $("#hours").text(sumHours)
-    }
-
-    // Here we will get the number of minutes in the week
-    let updateMinutes = function () {
-        let date = moment().weekday();
-        let weekMinutes = (date * 24 * 60) + (moment().minute());
-        $("#minutes").text(weekMinutes);
-    }
-
-    // Here we will get the number of seconds in the day
-    let updateSeconds = function () {
-        let todayHours = moment().hour();
-        let todaySeconds = todayHours * 60 * 60;
-        let thisSecond = moment().second();
-        let sumSeconds = thisSecond + todaySeconds;
-        $("#seconds").text(sumSeconds);
-    }
-
-    let countdown = function () {
-
-        // Friday
-        let friday = moment().day("fr")._d
-        
-        // convert this into day of the year
-        let fridayDate = moment(friday).dayOfYear();
-
-        // This will get the milliseconds of Friday
-        let fridayMils = fridayDate * 24 * 60 * 60 * 1000
-        // Turn into seconds
-
-        // Today
-        // Turn into milliseconds
-
-        let date = moment().dayOfYear()
-        let yearMils = date * 24 * 60 * 60 * 1000
-        let todayMils = moment().hour() * 60 * 60 * 1000;
-        let thisMilsSecond = moment().second() * 1000;
-        let thisMils = moment().millisecond();
-
-        // This value should be increasing over time
-        let sumMils = yearMils + todayMils + thisMilsSecond + thisMils;
-
-        let ms = fridayMils - sumMils;
-        $("#countdown").text(ms)
-    }
-
-    // To initally set the times, we will call the functions
-    updateTime();
-    updateHour();
-    updateMinutes();
-    updateSeconds();
-    countdown();
-
-    // To continuously call the functions, we will use setInterval
-    setInterval(updateTime, 1000);
-    setInterval(updateHour, 1000);
-    setInterval(updateMinutes, 1000);
-    setInterval(updateSeconds, 1000);
-    setInterval(countdown, 1);
-
-
-    $('.saveBtn').on("click",function(){
-      console.log(this);
-    });
-
-  //Stores specific time into the hour containers
-
-  var nineHour =  $("#nine-hour").val("9");
-  var tenHour =  $("#ten-hour").val("10");
-  var elevenHour = $("#eleven-hour").val("11");
-  var twelveHour = $("#twelve-hour").val("12");
-  var oneHour = $("#one-hour").val("13"); 
-  var twoHour = $("#two-hour").val("14");
-  var threeHour = $("#three-hour").val("15");
-  var fourHour = $("#four-hour").val("16");
-  var fiveHour = $("#five-hour").val("17");
+  localStorage.setItem(userText, userTime);
+});
   
-  let hourContainer = [
-    nineHour, tenHour, elevenHour, twelveHour,
-    oneHour, twoHour, threeHour, fourHour, fiveHour
-  ]
+
+//Stores specific time into the hour containers
+var nineHour =  $("#nine-hour").val("9");
+var tenHour =  $("#ten-hour").val("10");
+var elevenHour = $("#eleven-hour").val("11");
+var twelveHour = $("#twelve-hour").val("12");
+var oneHour = $("#one-hour").val("13"); 
+var twoHour = $("#two-hour").val("14");
+var threeHour = $("#three-hour").val("15");
+var fourHour = $("#four-hour").val("16");
+var fiveHour = $("#five-hour").val("17");
+  
+
+// Set class value into an array
+let hourContainer = [
+  nineHour, tenHour, elevenHour, twelveHour,
+  oneHour, twoHour, threeHour, fourHour, fiveHour
+]
 
   //  for(var i=0; i<=hourContainer.length; i++){
   //    console.log(hourContainer[i].val());
   //  }
 
-  var currentHour = moment().format("H");
-  console.log(currentHour);
-  
-  containerColor = function(){
-    for(var i=0; i<=hourContainer.length; i++){
-      if(hourContainer == currentHour){
-        hourContainer[i].addClass("present");
-      }else if(hourContainer > currentHour){
-        hourContainer[i].addClass("future")
-      }else{
-        hourContainer[i].addClass("past");
-      }
-    }
-  };
+  // setting current timer
+var currentHour = moment().format("H");
+console.log(currentHour);
+//compares current time to container times and adds color respectively 
+containerColor = function(){
+  for(var i=0; i<=hourContainer.length; i++){
+    if(hourContainer == currentHour){
+       hourContainer[i].addClass("present");
+    }else if(hourContainer > currentHour){
+      hourContainer[i].addClass("future")
+    }else{
+      hourContainer[i].addClass("past");
+     }
+  }
+};
 
-  containerColor();
-
-})
+containerColor();
